1b59f1260fecba3539506fd4651f797e7629e543
Wazid H. Ansari <wansar03@in.ibm.com>
2021-01-15 02:06:57 +0530

    Commented unnecerry code and remove localhost

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,19 @@
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data);
-    })
-})
+// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+//     response.json().then((data)=>{
+//         console.log(data);
+//     })
+// })
 
-fetch('/weather?address=Hyderabad').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-        console.log(data.error);
-        }else{
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    })
-})
+// fetch('/weather?address=Hyderabad').then((response)=>{
+//     response.json().then((data)=>{
+//         if(data.error){
+//         console.log(data.error);
+//         }else{
+//             console.log(data.location);
+//             console.log(data.forecast);
+//         }
+//     })
+// })
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
@@ -28,7 +28,7 @@ weatherForm.addEventListener('submit',(event)=>{
 
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             messageOne.textContent=data.error;
